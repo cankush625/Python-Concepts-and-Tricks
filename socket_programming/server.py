2e414e01ef2ae686f3c4f5181170df73b3cae7a9
cankush625 <cankush625@gmail.com>
2021-07-26 14:52:40 +0530

    Socket programming
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- socket_programming/server.py
+++ socket_programming/server.py
@@ -19,3 +19,10 @@ while True:
     client.send(b"Welcome to server")
 
     client.close()
+
+# Note:
+# The server socket doesn't send any data and server socket doesn't receives any data.
+# Server socket only produces the client sockets.
+# Each client socket is created in response to some other client socket doing a connect() to the
+# host and port we're bound to.
+# The two clients are free to chat it up.
